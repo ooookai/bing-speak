3cda55c27f6665db1b5f249eab42f57e7b850f41
ooookai <ooookai@gmail.com>
2017-02-25 22:50:34 +0800

    [x-audio] update audio control
    autoplay, play, paulse

--- src/x-audio.vue
+++ src/x-audio.vue
@@ -1,8 +1,13 @@
 <template>
-  <div id="audio">
-    <audio :src="audioSrc" autoplay controls>
+  <div>
+    <audio ref="audio" :src="audioSrc" :autoplay="autoplay" controls>
       您的瀏覽器不支援 audio 標籤功能。
     </audio>
+    <br>
+    <!-- <button @click="play()">play</button> -->
+    <!-- <button @click="pause()">pause</button> -->
+    <button @click="toggleAutoplay()">toggle autoplay</button>
+    <p v-show="autoplay">autoplay enabled</p>
   </div>
 </template>
 <script>
@@ -12,9 +17,27 @@ export default {
     audioSrc: String,
   },
   data() {
-    return {}
+    return {
+      autoplay: false,
+    }
   },
-  computed: {}
+  computed: {
+    audio() {
+      // audio doc: https://www.w3schools.com/TAGs/ref_av_dom.asp
+      return this.$refs.audio
+    }
+  },
+  methods: {
+    play() {
+      this.audio.play()
+    },
+    pause() {
+      this.audio.pause()
+    },
+    toggleAutoplay() {
+      this.autoplay = !this.autoplay
+    },
+  }
 }
 </script>
 <style>
